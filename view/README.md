0fc4eee7ed86cda0a4d5668a2422de8e7745ac3d
RafaelHR1 <101814500+RafaelHR1@users.noreply.github.com>
2023-06-14 13:53:19 +0700

    Add files via upload
    TUGAS PEMWEB VIEW

---
+++ view/README.md
@@ -0,0 +1,40 @@
+# Tugas HTML To Vue
+
+| Name  | NIM |
+| ----- | --- |
+| Fahrul Fuguh Verdian   | 21.11.4331  |
+| Gatan Inandarko | 21.11.4351 |
+| Jessica Vega Nainggolan | 21.11.4356  |
+| Jonathan Adi Saputro | 21.11.4359 |
+| Muhammad Jundi Robbani | 21.11.4363  |
+| Aldi Putra Miftaqul Ullumn | 21.11.4369  |
+| Ricardo Umbu Siwa Djurumanna Suyatno | 21.11.4380 |
+
+
+-----
+
+
+# vue-project
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ view/src/App.vue
@@ -0,0 +1,118 @@
+<template>
+  <header>
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <a class="navbar-brand" href="#"></a>
+                <img class="logo" src="./assets/logo.png" alt="">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+              <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li class="nav-item active">
+                  <a class="nav-link" href="#">Home <span class="sr-only ">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link disabled" href="#">Disabled</a>
+                </li>
+              </ul>
+              <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+              </form>
+            </div>
+          </nav>
+    </header>
+    <section class="hero">
+        <div class="container col-xxl-8 px-4 py-5">
+            <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+              <div class="col-10 col-sm-8 col-lg-6">
+                
+
+<lottie-player src="https://assets3.lottiefiles.com/packages/lf20_sk5h1kfn.json" background="transparent" speed="1"
+style="width: 100%; height: auto;" loop autoplay></lottie-player>
+              </div>
+              <div class="col-lg-6">
+                <h1 class="display-5 fw-bold lh-1 mb-3">Responsive left-aligned hero with image</h1>
+                <p class="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+                <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+                  <button type="button" class="btn btn-primary btn-lg px-4 me-md-2" fdprocessedid="r1q9el">Primary</button>
+                  <button type="button" class="btn btn-outline-secondary btn-lg px-4" fdprocessedid="pq0vp">Default</button>
+                </div>
+              </div>
+            </div>
+          </div>
+    </section>
+
+    <section class="produk text-center">
+        <div class="container">
+            <div class="align-items-center mb-3 row">
+                <div class="col-lg-12 ms-auto me-auto ">
+                    <h1 class="h1 mb-1 text-dark">
+                        Daftar Produk
+                    </h1>
+                    <p>Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s</p>
+                    <hr class="border border-succes border-2 opacity-50">
+                </div>
+                
+                <div class="row pt-5">
+                    <div class="col-lg-3 col-sm-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="80" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-airplay"><path d="M5 17H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-1"/><polygon points="12 15 17 21 7 21 12 15"/></svg>
+                        <h3 class="text-dark fw-normal pt-3">
+                            Produk 1
+                        </h3>
+                        <p>
+                            Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.
+                        </p>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="80" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-airplay"><path d="M5 17H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-1"/><polygon points="12 15 17 21 7 21 12 15"/></svg>
+                        <h3 class="text-dark fw-normal pt-3">
+                            Produk 1
+                        </h3>
+                        <p>
+                            Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.
+                        </p>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="80" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-airplay"><path d="M5 17H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-1"/><polygon points="12 15 17 21 7 21 12 15"/></svg>
+                        <h3 class="text-dark fw-normal pt-3">
+                            Produk 1
+                        </h3>
+                        <p>
+                            Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.
+                        </p>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="80" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-airplay"><path d="M5 17H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-1"/><polygon points="12 15 17 21 7 21 12 15"/></svg>
+                        <h3 class="text-dark fw-normal pt-3">
+                            Produk 1
+                        </h3>
+                        <p>
+                            Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </section>
+
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
